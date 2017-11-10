abce5eb6e93432efcc69c6447082dfb5fdcc1db7
Sergei Lebedev <tylubz@gmail.com>
2017-11-11 00:57:17 +0300

    update smack version and establish xmpp connection on device

--- app/src/main/java/net/tylubz/chat/activities/MainActivity.java
+++ app/src/main/java/net/tylubz/chat/activities/MainActivity.java
@@ -14,16 +14,27 @@ import net.tylubz.chat.R;
 import net.tylubz.chat.configuration.ConfigLoader;
 import net.tylubz.chat.configuration.Property;
 
-import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.AbstractXMPPConnection;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.SASLAuthentication;
+import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.chat2.Chat;
+import org.jivesoftware.smack.chat2.ChatManager;
 import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.sasl.SASLPlainMechanism;
+import org.jivesoftware.smack.sasl.core.SCRAMSHA1Mechanism;
+import org.jivesoftware.smack.sasl.provided.SASLPlainMechanism;
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+import org.jxmpp.jid.EntityBareJid;
+import org.jxmpp.jid.Jid;
+import org.jxmpp.jid.impl.JidCreate;
+import org.jxmpp.stringprep.XmppStringprepException;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
@@ -83,28 +94,54 @@ class MyTask extends AsyncTask<Void, Void, Void> {
     }
 
     private void establishConnection() throws XMPPException {
-        ConnectionConfiguration config = new ConnectionConfiguration(Property.HOST_NAME,
-                5222,
-                Property.SERVICE_NAME);
 
-        config.setSecurityMode(ConnectionConfiguration.SecurityMode.disabled);
-        config.setSocketFactory(new NoSSLv3SocketFactory());
-//        config.setSASLAuthenticationEnabled(false);
-        config.setServiceName("jabber.ru");
+        XMPPTCPConnectionConfiguration config = null;
+        try {
+            config = XMPPTCPConnectionConfiguration.builder()
+                    .setUsernameAndPassword(Property.USER_NAME, Property.PASSWORD)
+                    .setHost("jabber.ru")
+                    .setXmppDomain(JidCreate.domainBareFrom("jabber.ru"))
+                    .setPort(5222)
+//                    TODO adjust to secure way
+                    .setKeystoreType(null)
+//                    .setSecurityMode(ConnectionConfiguration.SecurityMode.disabled) // Do not disable TLS except for test purposes!
+                    .setDebuggerEnabled(true)
+                    .build();
+        } catch (XmppStringprepException e) {
+            e.printStackTrace();
+        }
 
-        // Внимание! Следующая строчка очень важна!
-//        SASLAuthentication.supportSASLMechanism("PLAIN");
-        XMPPConnection connection = new XMPPConnection(config);
+//        add SCRAM-SHA-1 mechanism for interaction
+        SASLAuthentication.registerSASLMechanism(new SCRAMSHA1Mechanism());
 
-        connection.connect();
-        connection.login(Property.USER_NAME, Property.PASSWORD); // т.е. не login@jabber.ru, а просто login
+        AbstractXMPPConnection connection;
+        connection = new XMPPTCPConnection(config);
+        try {
+            connection.connect().login();
+        } catch (SmackException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        Chat chat = connection.getChatManager().createChat(Property.RECIPIENT_NAME, new MessageListener() {
-            public void processMessage(Chat chat, Message message) {
-                System.out.println("Received message: " + message);
-            }
-        });
-        chat.sendMessage("Hello world");
+        ChatManager chatManager = ChatManager.getInstanceFor(connection);
+
+        EntityBareJid jid = null;
+        try {
+            jid = JidCreate.entityBareFrom("golub578@jabber.ru");
+        } catch (XmppStringprepException e) {
+            e.printStackTrace();
+        }
+        Chat chat = chatManager.chatWith(jid); // pass XmppClient instance as listener for received messages.
+        try {
+            chat.send("test");
+        } catch (SmackException.NotConnectedException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
 
