6fbf2d0f442143dbfb3e81147a339fb9668fd962
Sergei Lebedev <tylubz@gmail.com>
2017-12-02 23:20:30 +0300

    contact list activity: add multi choice, menu

--- app/src/main/java/net/tylubz/chat/contact_list/ContactListActivity.java
+++ app/src/main/java/net/tylubz/chat/contact_list/ContactListActivity.java
@@ -2,7 +2,14 @@ package net.tylubz.chat.contact_list;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import net.tylubz.chat.R;
@@ -13,22 +20,69 @@ import java.util.List;
 
 public class ContactListActivity extends AppCompatActivity {
 
-    String[] countries = { "Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
-    List<DummyContent> list = new ArrayList<>();
+    private String[] countries = { "Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
+    private List<DummyContent> list = new ArrayList<>();
+
+    private ListView countriesList;
+    private Button createChatButton;
+    private Button cancelChatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
 
-        ListView countriesList = (ListView) findViewById(R.id.countriesList);
+        createChatButton = (Button) findViewById(R.id.createChatButton);
+        cancelChatButton = (Button) findViewById(R.id.cancelChatButton);
 
-        // создаем адаптер
+        countriesList = (ListView) findViewById(R.id.countriesList);
+
+        // create adapter
         ArrayAdapter<String> adapter = new ArrayAdapter(this,
                 android.R.layout.simple_list_item_1, countries);
 
-        // устанавливаем для списка адаптер
         countriesList.setAdapter(adapter);
 
+        // add listener
+        countriesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                TODO extend
+                String selectedItem = countries[i];
+            }
+        });
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigation, menu);
+        return true;
+    }
+
+    public boolean addParticipants(MenuItem item) {
+        Log.i("info", "Item has been selected");
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_multiple_choice, countries);
+        createChatButton.setVisibility(View.VISIBLE);
+        cancelChatButton.setVisibility(View.VISIBLE);
+        countriesList.setAdapter(adapter);
+        return true;
+    }
+
+    public void onCancelButtonClick(View view) {
+        Log.i("info", "Cancel button has been pressed");
+        createChatButton.setVisibility(View.INVISIBLE);
+        cancelChatButton.setVisibility(View.INVISIBLE);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, countries);
+        countriesList.setAdapter(adapter);
+    }
+
+    public void onCreateButtonClick(View view) {
+        Log.i("info", "Create button has been pressed");
+    }
+
 }
---
+++ app/src/main/java/net/tylubz/chat/contact_list/model/UserInfo.java
@@ -0,0 +1,8 @@
+package net.tylubz.chat.contact_list.model;
+
+/**
+ * @author Sergei Lebedev
+ */
+
+public class UserInfo {
+}
